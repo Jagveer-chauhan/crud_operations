4e30f7be38a0da11920a025bca33833deb85b666
Jagveer-chauhan <jagveerchauhan3766@gmail.com>
2022-04-23 20:59:03 +0530

    Update settings.py

--- crud_opperations/settings.py
+++ crud_opperations/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'crud',
         'USER':'root',
-        'PASSWORD': 'jrc@123',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'port':'3306',
     }
